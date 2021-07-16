3f29aff09012dc1bec48a65337004c7afcc79348
Coding Campus <77583617+codingcampus4u@users.noreply.github.com>
2021-07-16 11:25:46 +0530

    Final Project View - Green House

---
+++ js/firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCWZhIVUCtSfru1h0rJCQqcirBttDAFAfo",
+    authDomain: "greenhouseprj-d1cda.firebaseapp.com",
+    projectId: "greenhouseprj-d1cda",
+    storageBucket: "greenhouseprj-d1cda.appspot.com",
+    messagingSenderId: "596423459307",
+    appId: "1:596423459307:web:25d3bcef459883eb8519ae",
+    measurementId: "G-B2K12SZ1FC"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
